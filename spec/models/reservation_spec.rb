b7435fa82475568a71cb9100adb3a67f9e2d499b
Dmitriy Kiriyenko <dmitriy.kiriyenko@gmail.com>
2014-06-19 15:20:25 +0300

    Dummy test.

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Reservation, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    context 'dates overlap' do
+      it 'checks overlap' do
+        expect(2*2).to eq(4)
+      end
+    end
+  end
 end
