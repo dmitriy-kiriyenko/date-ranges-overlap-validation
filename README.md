b3d6a9a9f0cb282cff0f6089f97d9af70d2ed9e8
Dmitriy Kiriyenko <dmitriy.kiriyenko@gmail.com>
2014-06-19 14:12:01 +0300

    Initial import.

---
+++ README.md
@@ -0,0 +1,1 @@
+# Date ranges overlap validations example
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails.application.config.session_store :cookie_store, key: '_date-ranges-overlap-validation-example_session'
