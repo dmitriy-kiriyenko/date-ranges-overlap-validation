0cf1a0108ae9452721273db3232f460c0cdb4fa5
Dmitriy Kiriyenko <dmitriy.kiriyenko@gmail.com>
2014-06-19 15:44:50 +0300

    First throw.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,16 @@
 class Reservation < ActiveRecord::Base
   belongs_to :table
+
+  validate :table_id, presence: true
+  validate :range_overlap
+
+  scope :overlaps, ->(interval) {
+    where.not(id: interval.id)
+      .where("date_part('epoch', (start_time - ?)) * date_part('epoch', (? - end_time)) >= 0",
+             interval.end_time, interval.start_time)
+  }
+
+  def range_overlap
+    errors.add(:base) if self.class.where(table_id: table_id).overlaps(self).any?
+  end
 end
--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -3,8 +3,27 @@ require 'rails_helper'
 RSpec.describe Reservation, :type => :model do
   context 'validations' do
     context 'dates overlap' do
+      let(:first_table) { Table.create! }
+      let(:second_table) { Table.create! }
+      let(:today) { Date.today }
+
+      def time_at(hours)
+        today.to_time + hours.hours
+      end
+
+      def reserve_at(range, table = first_table)
+        table.reservations.new(start_time: time_at(range.first),
+                               end_time: time_at(range.last))
+      end
+
+      it 'allows two reservations to co-exist' do
+        reserve_at(19..20).save!
+        expect(reserve_at(21..22)).to be_valid
+      end
+
       it 'checks overlap' do
-        expect(2*2).to eq(4)
+        reserve_at(19..21).save!
+        expect(reserve_at(20..22)).to_not be_valid
       end
     end
   end
